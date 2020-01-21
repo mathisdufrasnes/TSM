7710b14f98a1b4d1996313a50f5e685b1032d806
Mathis Dufrasnes <mathis.dufrasnes@hei.yncrea.fr>
2020-01-21 21:30:02 +0100

    First commit

---
+++ src/TSP.java
@@ -0,0 +1,45 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
+public class TSP {
+	public static  ArrayList<City> destinationCities = new ArrayList<City>();
+	public static void main(String[] args) {
+	        destinationCities.addAll(Arrays.asList(
+	        		new City(0,200,"Lisbon"),
+	        		new City(50,20,"Paris"),
+	        		new City(100,90,"London"),
+	        		new City(20,130,"Copenhague"),
+	        		new City(170,80,"Athens"),
+	        		new City(150,120,"Stockholm"),
+	        		new City(40,10,"Madrid"),
+	        		new City(80,70,"Roma"),
+	        		new City(150,30,"Berlin")));
+
+	        // Initialize population
+	      /*
+	       * 
+	       * Question 4-b: To complete
+	       * 
+	       */
+
+	        // Evolve population for 100 generations
+	      /*
+	       * 
+	       * Question 4-c: To complete
+	       * 
+	       * 
+	       */
+
+	        // Print final results
+		/*
+		 * 
+		 * Questions 4-d and 4-e: To Complete
+		 * 
+		 */
+	        
+	    }
+		
+		
+	
+
+}
